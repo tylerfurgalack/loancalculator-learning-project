b7198a16373305748a117f7deb3f8b4d90ff9ca5
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-07-19 14:05:42 -0400

    Loader & User Experience

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+    e.preventDefault()
+});
 
 // Calculate Resuslts
-function calculateResults(e) {
+function calculateResults() {
   console.log('Calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
@@ -24,15 +34,25 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * claculatedPayments).toFixed(2);
     totalInterest.value = ((monthly*claculatedPayments)-principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers')
   }
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
